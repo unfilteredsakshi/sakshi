f39cf7892e6cb9276404d9bcc8ac8b85d22739f9
unfilteredsakshi <sakshiaadesh2007@gmail.com>
2026-04-01 12:45:06 +0530

    ty
    ik

---
+++ Array.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main(){
+	int array[6]={10, 20, 30, 40, 50,60};
+	int i;
+	
+	printf("Element of the array are:\n");
+	
+
+    for (i=0; i< 5; i++){
+    	printf("array[%d]= %d\n",i, array[i]);
+    }
+    return 0;
+}
+	
+	
